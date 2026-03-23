0eb08576a59dc5b1f940ffead256302b40196e58
yuchunsheng.128 <yuchunsheng.128@bytedance.com>
2026-03-23 22:29:31 +0800

    refactor(extensions): merge las-toolkit and tos-tools into contextlake

--- extensions/contextlake/index.ts
+++ extensions/contextlake/index.ts
@@ -1,5 +1,10 @@
 import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
 import {
+  createLasSubmitTool,
+  createLasPollTool,
+  createLasGenericRequestTool,
+} from "./src/las-tools.js";
+import {
   createTosListObjectsTool,
   createTosPutObjectTool,
   createTosGetObjectTool,
@@ -7,10 +12,16 @@ import {
 } from "./src/tos-tools.js";
 
 export default definePluginEntry({
-  id: "tos-tools",
-  name: "TOS Tools Plugin",
-  description: "Bundled Volcengine TOS toolkit plugin",
+  id: "contextlake",
+  name: "ContextLake Toolkit Plugin",
+  description: "Bundled ContextLake (Volcengine LAS + TOS) toolkit plugin",
   register(api) {
+    // LAS Tools
+    api.registerTool(createLasSubmitTool(api) as any);
+    api.registerTool(createLasPollTool(api) as any);
+    api.registerTool(createLasGenericRequestTool(api) as any);
+
+    // TOS Tools
     api.registerTool(createTosListObjectsTool(api) as any);
     api.registerTool(createTosPutObjectTool(api) as any);
     api.registerTool(createTosGetObjectTool(api) as any);
